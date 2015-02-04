88911a77b9cbebe17fb882f938edf208c3fbbf09
David Adamo <me@davidadamojr.com>
2015-02-04 00:25:47 -0600

    removed android, chrome, changed font to support more languages

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <div class="row footer">
-	<p><a href="privacy.php" target="_blank">Privacy Policy</a></p>
+	<p>A project by <a href="http://twitter.com/davidadamojr" target="_blank">@davidadamojr</a><br/><br/><a href="privacy.php" target="_blank">Privacy Policy</a></p>
 </div>
